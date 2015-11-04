e2d709904247223f2d0cc00de2c04e6034238e14
Kevin Rose <kevin@maypark.com>
2015-11-04 00:32:27 -0600

    updating site with map example, image examples, and more

--- flask_builder/sitebuilder.py
+++ flask_builder/sitebuilder.py
@@ -38,7 +38,7 @@ freezer = Freezer(app)
 @app.route('/')
 def index():
     return render_template('index.html', pages=pages,
-        page={'title':''})
+        page={'title': 'factors'})
 
 @app.route('/projects/<path:path>/')
 def project_page(path):
@@ -48,21 +48,43 @@ def project_page(path):
 @app.route('/posts/<path:path>/')
 def posts_page(path):
     page = pages.get_or_404('posts/' + path)
-    return render_template('posts_page.html', page=page)
+    return render_template('post_page.html', page=page)
 
 @app.route('/tag/<string:tag>/')
 def tag(tag):
     tagged = [p for p in pages if tag in p.meta.get('tags', [])]
-    return render_template('tag.html', pages=tagged, tag=tag)
+    return render_template('tag.html', pages=tagged,
+        tag=tag, page={'title': 'tags'})
 
 @app.route('/img/<path:path>/')
 def img(path):
     return send_from_directory('img', path)
 
+@app.route('/js/<path:path>/')
+def js(path):
+    return send_from_directory('js', path)
+
 @app.route('/<path:path>/')
 def page(path):
     page = pages.get_or_404(path)
     return render_template('project_page.html', page=page)
+
+# freezer URLs
+
+@freezer.register_generator
+def product_url_generator():
+    # Return a list. (Any iterable type will do.)
+    path_to_js  = os.path.join(os.path.dirname(__file__),
+                               './js')
+    path_to_img = os.path.join(os.path.dirname(__file__),
+                              './img')
+
+    js_urls  = map(lambda x: '/js/' + x , os.listdir(path_to_js))
+    img_urls = map(lambda x: '/img/' + x, os.listdir(path_to_img))
+
+    return js_urls + img_urls
+
+
 
 if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1] == 'build':
