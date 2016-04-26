bb992263d126a6bc2764818787d9b64910281498
Kevin Rose <kevin+gh@maypark.com>
2016-04-25 23:35:40 -0500

    cleaning up master

--- flask_builder/sitebuilder.py
+++ flask_builder/sitebuilder.py
@@ -1,10 +1,12 @@
 import sys
 import os
+import argparse
 
-from flask import Flask, render_template, render_template_string, send_from_directory
-from flask_flatpages import FlatPages
-from flask_flatpages.utils import pygmented_markdown
-from flask_frozen import Freezer
+# Flask imports
+import flask
+import flask_flatpages
+import flask_flatpages.utils
+import flask_frozen
 
 # Flask configurations
 DEBUG = True
@@ -21,53 +23,53 @@ FREEZER_REMOVE_EXTRA_FILES = False
 # Utils
 
 def jinja_prerender(text):
-    prerendered_body = render_template_string(text)
+    prerendered_body = flask.render_template_string(text)
     print(text)
-    return pygmented_markdown(prerendered_body)
+    return flask_flatpages.utils.pygmented_markdown(prerendered_body)
 
 # Set up
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 app.config.from_object(__name__)
 app.config['FLATPAGES_HTML_RENDERER'] = jinja_prerender
-pages = FlatPages(app)
-freezer = Freezer(app)
+pages = flask_flatpages.FlatPages(app)
+freezer = flask_frozen.Freezer(app)
 
 # Route methods
 
 @app.route('/')
 def index():
-    return render_template('index.html', pages=pages,
+    return flask.render_template('index.html', pages=pages,
         page={'title': 'factors'})
 
 @app.route('/projects/<path:path>/')
 def project_page(path):
     page = pages.get_or_404('projects/' + path)
-    return render_template('project_page.html', page=page)
+    return flask.render_template('project_page.html', page=page)
 
 @app.route('/posts/<path:path>/')
 def posts_page(path):
     page = pages.get_or_404('posts/' + path)
-    return render_template('post_page.html', page=page)
+    return flask.render_template('post_page.html', page=page)
 
 @app.route('/tag/<string:tag>/')
 def tag(tag):
     tagged = [p for p in pages if tag in p.meta.get('tags', [])]
-    return render_template('tag.html', pages=tagged,
+    return flask.render_template('tag.html', pages=tagged,
         tag=tag, page={'title': 'tags'})
 
 @app.route('/img/<path:path>/')
 def img(path):
-    return send_from_directory('img', path)
+    return flask.send_from_directory('img', path)
 
 @app.route('/js/<path:path>/')
 def js(path):
-    return send_from_directory('js', path)
+    return flask.send_from_directory('js', path)
 
 @app.route('/<path:path>/')
 def page(path):
     page = pages.get_or_404(path)
-    return render_template('project_page.html', page=page)
+    return flask.render_template('project_page.html', page=page)
 
 # freezer URLs
 
@@ -84,10 +86,26 @@ def product_url_generator():
 
     return js_urls + img_urls
 
+# command line functions
+def build(args):
+    freezer.freeze()
+    pass
+
+def run(args):
+    app.run(port=args.port)
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1 and sys.argv[1] == 'build':
-        freezer.freeze()
-    else:
-        app.run(port=8000)
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers()
+
+    build_parser = subparsers.add_parser('build')
+
+    run_parser = subparsers.add_parser('run')
+    run_parser.add_argumnent('-p', '--port',
+                             help='Specify port',
+                             nargs=1,
+                             type=int,
+                             default=8000)
+
+    args = parser.parse_args()
